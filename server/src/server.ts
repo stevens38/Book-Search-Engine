c40935c3defd869a09f59d0078d9c91a4a5bbddb
JStevens <stevens38@marshall.edu>
2024-12-30 19:08:33 -0500

    server.ts updated with apollo

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,21 +1,48 @@
 import express from 'express';
 import path from 'node:path';
 import db from './config/connection.js';
-import routes from './routes/index.js';
+// import routes from './routes/index.js';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { typeDefs, resolvers } from './schemas/index.js';
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const startApolloServer = async () => {
+  await server.start();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use('/graphql', expressMiddleware(server));
+
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
+
+  app.get('*', (_req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
 }
 
-app.use(routes);
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.listen(PORT, () => {
+  console.log(`API server running on port ${PORT}!`);
+  console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+});
+};
+
+startApolloServer();
+
+/*app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+});  */
